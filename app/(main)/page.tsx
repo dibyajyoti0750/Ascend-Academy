149d419d6defe96c1cc6fb4e7c536b9120bc16c1
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-05-12 12:08:16 +0530

    testimonials section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,15 @@
-import { dummyCourses } from "@/assets/assets";
 import Companies from "@/components/student/Companies";
 import CoursesSection from "@/components/student/CoursesSection";
 import Hero from "@/components/student/Hero";
+import TestimonialsSection from "@/components/student/TestimonialsSection";
 
 export default function Page() {
   return (
     <main className="overflow-hidden">
       <Hero />
       <Companies />
-      <CoursesSection courses={dummyCourses} />
+      <CoursesSection />
+      <TestimonialsSection />
     </main>
   );
 }
